11a786e92f6aa72639e0f9bd15891e013d551793
Rodrigo Chin <rodrigo.chin@outlook.com>
2021-08-14 11:31:42 -0300

    Implement binary tree properties

--- BinaryTree/BinaryTree.cpp
+++ BinaryTree/BinaryTree.cpp
@@ -1,4 +1,5 @@
 #include <stdbool.h>
+#include <stdlib.h>
 
 #pragma region Structure
 
@@ -35,5 +36,56 @@ void PrintTreePreOrder(Tree tree);
 void ShowPaths(Tree tree);
 void ShowTree(Tree tree, int height = 0);
 void Remove(Tree *tree, TKey key);
+
+#pragma endregion
+
+Tree Initialize()
+{
+  return NULL;
+}
+
+int main()
+{
+  Tree tree = Initialize();
+
+  system("pause");
+}
+
+#pragma region Properties
+
+int Count(Tree tree)
+{
+  return tree == NULL ? 0 : 1 + Count(tree->left) + Count(tree->right);
+}
+
+int CountLeaf(Tree tree)
+{
+  if (tree == NULL)
+    return 0;
+
+  if (Count(tree->left) + Count(tree->right) == 0)
+    return 1;
+
+  return CountLeaf(tree->left) + CountLeaf(tree->right);
+}
+
+int CountNonLeaf(Tree tree)
+{
+  if (tree == NULL || (Count(tree->left) + Count(tree->right)) == 0)
+    return 0;
+
+  return 1 + CountNonLeaf(tree->left) + CountNonLeaf(tree->right);
+}
+
+int Height(Tree tree)
+{
+  if (tree == NULL)
+    return 0;
+
+  int leftHeight = Height(tree->left);
+  int rightHeight = Height(tree->right);
+
+  return (leftHeight > rightHeight ? leftHeight : rightHeight) + 1;
+}
 
 #pragma endregion
