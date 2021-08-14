f77f328e3efc987a090fe84b25e581a4cbbee4a1
Rodrigo Chin <rodrigo.chin@outlook.com>
2021-08-14 11:38:40 -0300

    Implement binary tree methods

--- BinaryTree/BinaryTree.cpp
+++ BinaryTree/BinaryTree.cpp
@@ -1,5 +1,7 @@
 #include <stdbool.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include <strings.h>
 
 #pragma region Structure
 
@@ -86,6 +88,169 @@ int Height(Tree tree)
   int rightHeight = Height(tree->right);
 
   return (leftHeight > rightHeight ? leftHeight : rightHeight) + 1;
+}
+
+#pragma endregion
+
+#pragma region Methods
+
+/* TODO: add balanced search tree logic */
+void Add(Tree *tree, TKey key, TValue value)
+{
+  if (*tree == NULL)
+  {
+    *tree = (Tree)malloc(sizeof(Node));
+    (*tree)->value = (TValue)malloc(strlen(value));
+    (*tree)->left = NULL;
+    (*tree)->right = NULL;
+    (*tree)->key = key;
+    strcpy((*tree)->value, value);
+  }
+  else if (key < (*tree)->key)
+  {
+    Add(&(*tree)->left, key, value);
+  }
+  else if (key > (*tree)->key)
+  {
+    Add(&(*tree)->right, key, value);
+  }
+  else
+  {
+    printf("Node already exists!\n");
+  }
+}
+
+void Clear(Tree *tree)
+{
+  if (*tree == NULL)
+    return;
+
+  Clear(&(*tree)->left);
+  Clear(&(*tree)->right);
+
+  free(*tree);
+  *tree = NULL;
+}
+
+bool Contains(Tree tree, TKey key)
+{
+  if (tree == NULL)
+    return false;
+
+  if (tree->key == key)
+    return true;
+
+  return key < tree->key
+             ? Contains(tree->left, key)
+             : Contains(tree->right, key);
+}
+
+Node *Find(Tree tree, TKey key)
+{
+  if (tree == NULL)
+    return NULL;
+
+  if (tree->key == key)
+    return tree;
+
+  return key < tree->key
+             ? Find(tree->left, key)
+             : Find(tree->right, key);
+}
+
+Node *FindValue(Tree tree, const TValue value)
+{
+  if (tree == NULL)
+    return NULL;
+
+  if (strcmp(value, tree->value) == 0)
+    return tree;
+
+  Node *temp = FindValue(tree->left, value);
+
+  if (temp == NULL)
+    temp = FindValue(tree->right, value);
+
+  return temp;
+}
+
+void PrintNode(Node *node)
+{
+  printf("* %i %s\n", node->key, node->value);
+}
+
+void PrintTreeInOrder(Tree tree)
+{
+  if (tree != NULL)
+  {
+    PrintTreeInOrder(tree->left);
+    PrintNode(tree);
+    PrintTreeInOrder(tree->right);
+  }
+}
+
+void PrintTreePostOrder(Tree tree)
+{
+  if (tree != NULL)
+  {
+    PrintTreePostOrder(tree->left);
+    PrintTreePostOrder(tree->right);
+    PrintNode(tree);
+  }
+}
+
+void PrintTreePreOrder(Tree tree)
+{
+  if (tree != NULL)
+  {
+    PrintNode(tree);
+    PrintTreePreOrder(tree->left);
+    PrintTreePreOrder(tree->right);
+  }
+}
+
+/* TODO: implement using queue */
+void ShowPathsAuxiliar(Tree tree, TKey paths[], int height = 0)
+{
+  if (tree != NULL)
+  {
+    paths[height] = tree->key;
+    height++;
+
+    if (tree->left == NULL && tree->right == NULL)
+    {
+      for (int i = 0; i < height; i++)
+        printf("%i ", paths[i]);
+
+      printf("\n");
+    }
+    else
+    {
+      ShowPathsAuxiliar(tree->left, paths, height);
+      ShowPathsAuxiliar(tree->right, paths, height);
+    }
+  }
+}
+
+void ShowPaths(Tree tree)
+{
+  int size = Height(tree);
+  TKey *paths = (TKey *)malloc(size * sizeof(TKey));
+  ShowPathsAuxiliar(tree, paths);
+}
+
+void ShowTree(Tree tree, int height)
+{
+  if (tree != NULL)
+  {
+    ShowTree(tree->left, height + 1);
+
+    for (int i = 0; i < height; i++)
+      printf("\t");
+    PrintNode(tree);
+
+    ShowTree(tree->right, height + 1);
+  }
 }
 
 #pragma endregion
